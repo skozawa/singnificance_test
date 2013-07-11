cc015e5d442eb15d1a24f1c685223e24d60533d1
skozawa <argentcrescent@gmail.com>
2013-07-12 01:07:21 +0900

    modify data

--- chi_square.pl
+++ chi_square.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Statistics::ChisqIndep;
 
-my $data = [ [350, 1000], [300, 1000] ];
+my $data = [ [350, 650], [300, 700] ];
 my $significance = 95;
 
 my $chi = Statistics::ChisqIndep->new;
@@ -12,3 +12,4 @@ $chi->load_data($data);
 my $prob = sprintf("%2.4f", (1 - $chi->{p_value}) * 100);
 say $prob > $significance ? 'significant' : 'not significant';
 say $prob , '%';
+

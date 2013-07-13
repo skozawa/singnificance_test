2262eaf85f85de1d97c8716897adb8c7d45d4264
skozawa <skozawa@hatena.ne.jp>
2013-07-13 12:30:30 +0900

    reverse data

--- interval_estimation.pl
+++ interval_estimation.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Statistics::PointEstimation;
 
-my $data = [ [350, 650], [300, 700] ];
+my $data = [ [300, 700], [350, 650] ];
 my $significance = $ARGV[0] // 95;
 
 my $a_stats = stats($data->[0]);
@@ -12,7 +12,7 @@ my $b_stats = stats($data->[1]);
 printf("%2.4f%%\t%2.4f%%\t%2.4f%%\n", $a_stats->lower_clm * 100, $a_stats->mean * 100, $a_stats->upper_clm * 100);
 printf("%2.4f%%\t%2.4f%%\t%2.4f%%\n", $b_stats->lower_clm * 100, $b_stats->mean * 100, $b_stats->upper_clm * 100);
 
-say $a_stats->lower_clm > $b_stats->upper_clm ? 'significant' : 'not significant';
+say $b_stats->lower_clm > $a_stats->upper_clm ? 'significant' : 'not significant';
 
 sub stats {
     my $data = shift;
@@ -28,11 +28,11 @@ sub stats {
 }
 
 # perl interval_estimation.pl 95
-# 32.0402%        35.0000%        37.9598%
 # 27.1564%        30.0000%        32.8436%
+# 32.0402%        35.0000%        37.9598%
 # not significant
 
 # perl interval_estimation.pl 90
-# 32.5167%        35.0000%        37.4833%
 # 27.6141%        30.0000%        32.3859%
+# 32.5167%        35.0000%        37.4833%
 # significant
